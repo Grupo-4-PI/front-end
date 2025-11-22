007563e737e7ac41e4e18098e29b4bd936009e99
FernandoRDev457 <fernando.gutierrez@sptech.school>
2025-11-22 15:02:56 -0300

    feat: Implementando dados dinamicos kpi grupo problema e tmr score

--- web-data-viz/public/screens/dashboard/dash-benchmark/graphics.js
+++ web-data-viz/public/screens/dashboard/dash-benchmark/graphics.js
@@ -40,6 +40,8 @@ function buscarBenchmark(mes) {
         .then(dados => {
             console.log("Benchmark:", dados);
             montarGraficos(dados);
+            atualizarTMR(dados)
+            atualizarKpiGrupoProblema(dados);
         })
         .catch(err => {
             console.error("Erro ao buscar benchmark:", err);
@@ -123,6 +125,70 @@ function montarGraficos(dados) {
             scales: { y: { min: 0, max: 5 } }
         }
     });
+}
+
+
+// 5. ------ ATUALIZAR KPI DE TMR -------
+function atualizarTMR(dados) {
+    if (!dados.tmr || dados.tmr.length === 0) return;
+
+    const tmrEmpresa = Number(dados.tmr[0].tmr_empresa);
+    const tmrMercado = Number(dados.tmr[0].tmr_mercado);
+
+    // converter decimal para Horas + Minutos
+    function converterHM(valor) {
+        const horas = Math.floor(valor);
+        const minutos = Math.round((valor - horas) * 60);
+        return `${horas}h ${String(minutos).padStart(2, "0")}min`;
+    }
+
+    document.querySelector(".gauge.company h2").textContent = converterHM(tmrEmpresa);
+    document.querySelector(".gauge.market h2").textContent = converterHM(tmrMercado);
+
+    function calcularPercentual(valor) {
+        const min = 2;
+        const max = 20;
+        let perc = ((valor - min) / (max - min)) * 100;
+
+        if (perc < 0) perc = 0;
+        if (perc > 100) perc = 100;
+        return perc;
+    }
+
+    const percEmpresa = calcularPercentual(tmrEmpresa);
+    const percMercado = calcularPercentual(tmrMercado);
+
+    function percentualParaGraus(perc) {
+        return (perc * 180) / 100;
+    }
+
+    const degEmpresa = percentualParaGraus(percEmpresa);
+    const degMercado = percentualParaGraus(percMercado);
+
+    document.querySelector(".gauge-arc.company").style.setProperty(
+        "--gauge-rotation",
+        `${degEmpresa}deg`
+    );
+
+    document.querySelector(".gauge-arc.mart").style.setProperty(
+        "--gauge-rotation",
+        `${degMercado}deg`
+    );
+}
+
+
+// 6. ------ KPIS DE MAIOR E MENOR GRUPO PROBLEMA --------
+function atualizarKpiGrupoProblema(dados) {
+    const kpis_grupo = document.querySelectorAll(".type-problem");
+    const kpis_score = document.querySelectorAll(".data-score p");
+
+    dados.gruposProblemas.forEach((grupo, index) => {
+        kpis_grupo[index].textContent = grupo.grupo_problema;
+
+        let media = Number(grupo.media_empresa).toFixed(2);
+        kpis_score[index].textContent = media + "/5"
+    });
+
 }
 
 function inicializarBenchmark() {
